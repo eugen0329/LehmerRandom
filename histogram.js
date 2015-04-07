88503a79598ced738b8b642d0703915ca57c7c79
eugen0329 <eugen0329@gmail.com>
2015-04-07 19:53:18 +0000

    Histogram appending extracted as a separate function

---
+++ histogram.js
@@ -0,0 +1,51 @@
+function appendHistogram(container, values, nBars, size, margin) {
+  var valuesCount = values.length;
+
+  var x = d3.scale.linear()
+      .domain([0, 1])
+      .range([0, size.width]),
+    xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var data = d3.layout.histogram()
+    .bins(x.ticks(nBars))(values)
+    .map(function(a) {
+      a.y /= valuesCount;
+      return a;
+    });
+
+  var y = d3.scale.linear()
+      .domain([0, 1.5 * d3.max(data, function(d) { return d.y; })])
+      .range([size.height, 0]),
+    yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select(container).append("svg")
+    .attr("width", size.width + margin.left + margin.right)
+    .attr("height", size.height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+    .data(data)
+    .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+  bar.append("rect")
+    .attr("x", 1)
+    .attr("width", x(data[0].dx) - 1)
+    .attr("height", function(d) { return size.height - y(d.y); });
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + size.height + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(0,0)")
+    .call(yAxis);
+}
--- main.js
+++ main.js
@@ -8,65 +8,21 @@ $(document).ready(function() {
   };
 
   var margin = {top: 10, right: 30, bottom: 30, left: 30},
-      width  = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+      size = {
+        width: 960 - margin.left - margin.right,
+        height: 500 - margin.top - margin.bottom
+      };
 
   var randNumCount = DEFAULT.randNumCount,
     initial        = DEFAULT.initial,
     multiplier     = DEFAULT.multiplier,
     divider        = DEFAULT.divider,
-    barsCount      = DEFAULT.barsCount;
+    nBars          = DEFAULT.barsCount;
 
   var rand = new LehmerRand(initial, multiplier, divider);
 
   var values = d3.range(randNumCount).map(rand);
+  appendHistogram('.histogram-container', values, nBars, size, margin);
 
-  var x = d3.scale.linear()
-      .domain([0, 1])
-      .range([0, width]),
-    xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var data = d3.layout.histogram()
-    .bins(x.ticks(barsCount))(values)
-    .map(function(a) {
-      a.y /= randNumCount;
-      return a;
-    });
-
-  var y = d3.scale.linear()
-      .domain([0, 1.5 * d3.max(data, function(d) { return d.y; })])
-      .range([height, 0]),
-    yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-
-  var svg = d3.select(".histogram-container").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  var bar = svg.selectAll(".bar")
-    .data(data)
-    .enter().append("g")
-    .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-
-  bar.append("rect")
-    .attr("x", 1)
-    .attr("width", x(data[0].dx) - 1)
-    .attr("height", function(d) { return height - y(d.y); });
-
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-  svg.append("g")
-    .attr("class", "y axis")
-    .attr("transform", "translate(0,0)")
-    .call(yAxis);
 });
 
