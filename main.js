92127f8c9d4b3f3d816bc1bd21a94d763cd10edc
eugen0329 <eugen0329@gmail.com>
2015-04-07 22:18:43 +0000

    Added basic user interface, implemented tabs for other destributions

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ $(document).ready(function() {
     randNumCount:  1000000,
     barsCount:     20
   };
+  //$('.tabs-panel a[href="#tab1"]').click();
 
-  var margin = {top: 10, right: 30, bottom: 30, left: 30},
+  var margin = {top: 10, right: 30, bottom: 30, left: 40},
       size = {
-        width: 960 - margin.left - margin.right,
-        height: 500 - margin.top - margin.bottom
+        width: $('.histogram-container').width() - margin.left - margin.right,
+        height: $('.histogram-container').height() - margin.top - margin.bottom
       };
 
   var randNumCount = DEFAULT.randNumCount,
@@ -22,6 +23,18 @@ $(document).ready(function() {
   var rand = new LehmerRand(initial, multiplier, divider);
 
   var values = d3.range(randNumCount).map(rand);
-  appendHistogram('.histogram-container', values, nBars, size, margin);
+  appendHistogram('.uniform-destribution .histogram-container', values, nBars, size, margin);
+
+  bindEventHandlers();
 });
 
+
+function bindEventHandlers() {
+  $('.tabs-panel a').on('click', function(e) {
+      var currentAttrValue = $(this).attr('href');
+      $('.tabs-wrapper ' + currentAttrValue).fadeIn(400).siblings().hide();
+      $(this).parent('li').addClass('active').siblings().removeClass('active');
+      $('.tabs-wrapper ' + currentAttrValue + 'submit-btn').click();
+      e.preventDefault();
+  });
+}
