663df5c2e0da14eb04492bed1039e24c618910a0
eugen0329 <eugen0329@gmail.com>
2015-04-08 18:16:08 +0000

    Added gauss distribution implementation, written more pathes for the Number and Array

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ var DEFAULT = {
   barsCount:     20
 };
 
+
+
 $(document).ready(function() {
 
   var margin = {top: 10, right: 30, bottom: 30, left: 40},
@@ -15,18 +17,17 @@ $(document).ready(function() {
         height: $('.histogram-container').height() - margin.top - margin.bottom
       };
 
-  var valuesCount  = DEFAULT.valuesCount,
+  var valuescount  = DEFAULT.valuescount,
     initial        = DEFAULT.initial,
     multiplier     = DEFAULT.multiplier,
     divider        = DEFAULT.divider;
 
-  rand = new LehmerRand(initial, multiplier, divider);
+  rand = LCRand(initial, multiplier, divider);
 
-
-  bindEventHandlers(size, margin);
+  bindeventhandlers(size, margin);
 });
 
-function bindEventHandlers(size, margin) {
+function bindeventhandlers(size, margin) {
   $('.tabs-panel a').on('click', function(e) {
       var currentAttrValue = $(this).attr('href');
       $('.tabs-wrapper ' + currentAttrValue).fadeIn(400).siblings().hide();
@@ -36,11 +37,11 @@ function bindEventHandlers(size, margin) {
   });
 
   $('.uniform-distribution .inp-form').on('submit', function() {
-    var a = parseFloat($('.inp-a', this).val());
-    var b = parseFloat($('.inp-b', this).val());
-    var valuesCount = parseInt($('.inp-n', this).val());
-    var nBars = parseInt($('.inp-k', this).val());
-    var target = '.uniform-distribution .histogram-container';
+    var a           = parseFloat($('.inp-a', this).val());
+    var b           = parseFloat($('.inp-b', this).val());
+    var valuescount = parseInt($('.inp-nvals', this).val());
+    var nBars       = parseInt($('.inp-nbars', this).val());
+    var target      = '.uniform-distribution .histogram-container';
 
     var mx = (a + b) / 2;
     $('.uniform-distribution .mx .value').html(mx);
@@ -49,12 +50,37 @@ function bindEventHandlers(size, margin) {
     var sd = Math.sqrt(dx);
     $('.uniform-distribution .sd .value').html(sd);
 
-    var values = d3.range(valuesCount).map(function() {
-      return a + (b - a) * rand();
-    });
+    var values = d3.range(valuescount).map(rand.uniform(a, b));
 
     $(target).empty();
-    var xDomain = expandRange([a,b], 0.2);
+    var xdomain = expandRange([a,b], 0.2);
+    appendHistogram(target, values, xdomain, nBars, size, margin);
+
+    return false;
+  });
+
+  $('.gauss-distribution .inp-form').on('submit', function() {
+    var mx = parseFloat($('.inp-mx', this).val());
+    var sd = parseFloat($('.inp-sd', this).val());
+    var n = parseFloat($('.inp-n', this).val());
+    var valuesCount = parseInt($('.inp-val-count', this).val());
+    var nBars = parseInt($('.inp-nbars', this).val());
+    var target = '.gauss-distribution .histogram-container';
+
+    var values = d3.range(valuesCount).map(rand.gauss(mx, sd, n));
+
+    mx = values.sum() / values.length;
+    $('.gauss-distribution .mx .value').html(mx);
+    var dx = values.reduce(function(sum, cur) {
+      return sum + (cur - mx).square() / (values.length - 1);
+    }, 0);
+    $('.gauss-distribution .dx .value').html(dx);
+    sd = Math.sqrt(dx);
+    $('.gauss-distribution .sd .value').html(sd);
+
+    $(target).empty();
+    var xDomain = [0, d3.max(values)];
+    values = values.keepDomain(xDomain);
     appendHistogram(target, values, xDomain, nBars, size, margin);
 
     return false;
