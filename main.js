7165ae2efafdbc603ee8c5f25fc0a79d8c122835
eugen0329 <eugen0329@gmail.com>
2015-04-08 23:32:04 +0000

    Extracted distribution params function

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var rand;
 var DEFAULT = {
   initial:       27211.0,
   multiplier:    48271.0,
@@ -7,13 +6,31 @@ var DEFAULT = {
   barsCount:     20
 };
 
-$(document).ready(function() {
+var PARAMS = {
+  uniform: {
+    a:       { selector: '.inp-a',     parser: parseFloat},
+    b:       { selector: '.inp-b',     parser: parseFloat},
+    nBars:   { selector: '.inp-nbars', parser: parseInt},
+    nValues: { selector: '.inp-nvals', parser: parseInt}
+  },
+  gauss: {
+    mx: { selector: '.inp-mx', parser: parseFloat},
+    sd: { selector: '.inp-sd', parser: parseFloat},
+    n: { selector: '.inp-n', parser: parseInt},
+    nValues: { selector: '.inp-nvals', parser: parseInt},
+    nBars: { selector: '.inp-nbars', parser: parseInt}
+  }
 
-  var margin = {top: 10, right: 30, bottom: 30, left: 40},
-      size = {
-        width: $('.histogram-container').width() - margin.left - margin.right,
-        height: $('.histogram-container').height() - margin.top - margin.bottom
-      };
+};
+
+var rand, margin, size;
+
+$(document).ready(function() {
+  margin = {top: 10, right: 30, bottom: 30, left: 40};
+  size = {
+    width: $('.histogram-container').width() - margin.left - margin.right,
+    height: $('.histogram-container').height() - margin.top - margin.bottom
+  };
 
   var valuescount  = DEFAULT.valuescount,
     initial        = DEFAULT.initial,
@@ -35,48 +52,48 @@ function bindeventhandlers(size, margin) {
   });
 
   $('.uniform-distribution .inp-form').on('submit', function() {
-    var a           = parseFloat($('.inp-a', this).val());
-    var b           = parseFloat($('.inp-b', this).val());
-    var valuescount = parseInt($('.inp-nvals', this).val());
-    var nBars       = parseInt($('.inp-nbars', this).val());
-    var target      = '.uniform-distribution';
+    var target  = '.uniform-distribution';
+    var p      = getParams(PARAMS.uniform, this);
+    var values = d3.range(p.nValues).map(rand.uniform(p.a, p.b));
+    var xDomain = expandRange([p.a, p.b], 0.2);
 
-    var values = d3.range(valuescount).map(rand.uniform(a, b));
     appendEvaluations(values, target);
-
-    var xDomain = expandRange([a,b], 0.2);
-    appendHistogram(target + ' .histogram-container', values, xDomain, nBars, size, margin);
+    appendHistogram(target + '.histogram-container', values, xDomain, p.nBars, size, margin);
 
     return false;
   });
 
   $('.gauss-distribution .inp-form').on('submit', function() {
-    var mx = parseFloat($('.inp-mx', this).val());
-    var sd = parseFloat($('.inp-sd', this).val());
-    var n = parseFloat($('.inp-n', this).val());
-    var valuesCount = parseInt($('.inp-nvals', this).val());
-    var nBars = parseInt($('.inp-nbars', this).val());
     var target = '.gauss-distribution';
-
-    var values = d3.range(valuesCount).map(rand.gauss(mx, sd, n));
-    appendEvaluations(values, target);
-
+    var p = getParams(PARAMS.gauss);
+    var values = d3.range(p.nValues).map(rand.gauss(p.mx, p.sd, p.n));
     var xDomain = [0, d3.max(values)];
+
+    appendEvaluations(values, target);
     values = values.keepDomain(xDomain);
-    appendHistogram(target + ' .histogram-container', values, xDomain, nBars, size, margin);
+    appendHistogram(target + ' .histogram-container', values, xDomain, p.nBars, size, margin);
 
     return false;
   });
+}
 
+function getParams(list, object) {
+  var params = {};
+  $.each(list, function(name, p) {
+    params[name] = p.parser($(p.selector, object).val());
+  });
+
+  return params;
 }
 
 function appendEvaluations(values, target) {
   var mx = values.sum() / values.length;
-  $(target + ' .mx .value').html(mx);
   var dx = values.reduce(function(sum, cur) {
     return sum + (cur - mx).square() / (values.length - 1);
   }, 0);
-  $(target + ' .dx .value').html(dx);
   var sd = Math.sqrt(dx);
+
+  $(target + ' .mx .value').html(mx);
+  $(target + ' .dx .value').html(dx);
   $(target + ' .sd .value').html(sd);
 }
